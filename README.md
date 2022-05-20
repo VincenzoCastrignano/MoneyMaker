76118462ca7e93c9b8a61b59310ec4680148626e
VincenzoCastrignano <vincenzo.castrignano@esqese.ucly.fr>
2022-05-20 08:37:24 +0200

    Ajout explication README

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # MoneyMaker
 
 ## Repo for my money maker application
+
+
+### Explications
+
+Dans le fichier de base lors de la génération de la fenêtre :
+```html
+Principe à demander :
+    - Avoir la fenêtre de base qui est générée seule, lorsque que nous
+utilisons un bouton, cela change le nom de la fenêtre et en fonction du nom,
+On utilise une interface personnalisée pour chaque fenêtre.
+Interface Base :
+    - fenêtre de base avec un bouton de connexion ou d'inscription
+    Ou d'ajout de compte supplémentaire (bonus)
+    - Le bouton inscription : fonction Create_User()
+    - Le bouton connexion : fonction Test_User() A créer
+    - Le bouton Ajout de compte : fonction add_User() A créer
+    Interface Inscription :
+        - Affichage d'un form pour insérer ses données
+        - Bouton Valider qui test si les données du form sont correctes
+        - Bouton Valider Ouvre l'interface utilisateur
+    Interface Connexion :
+        - Affichage d'un form pour insérer le pseudo et le mdp
+        - Si correcte, envoie interface utilisateur
+        - Si incorrecte, Renvoie sur la fenêtre de base
+
+```
--- subClass/App.py
+++ subClass/App.py
@@ -1,24 +1,48 @@
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from random import choice
+import sys
+
+window_titles = [
+    "Base",
+    "Connexion",
+    "Inscription",
+    "Ajout Utilisateur",
+    "Accueil",
+    'Something went wrong'
+]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.n_time_clicked = 0
+
         self.setWindowTitle("Business")
+
         self.setFixedSize(QSize(400, 300))
 
         self.button = QPushButton("Clique ici!")
         self.button.clicked.connect(self.the_button_was_clicked)
 
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+
         self.setCentralWidget(self.button)
 
     def the_button_was_clicked(self):
-        self.button.setText("Good job bro.")
-        self.button.setEnabled(False)
+        print("Clique !")
+        new_window_title = choice(window_titles)
+        print("Paramètre du titre : %s" % new_window_title)
 
         # Also change the window title.
-        self.setWindowTitle("Mon nouveau Business")
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print("Paramètre de fenêtre switch : %s" % window_title)
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
+
 
 class App:
     N_User: 0
@@ -43,4 +67,3 @@ class App:
     def Limit_User(self, N_User):
         if N_User > 10:
             print("Trop de User !")
-
